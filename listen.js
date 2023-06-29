c039aa0fa91c61f0df2960c4cb73c92229e9f8a7
James Yuill <jamesyuill@jamess-mbp.lan>
2023-06-29 10:52:57 +0100

    removes a bracket

--- listen.js
+++ listen.js
@@ -2,6 +2,5 @@ const app = require('./serverFiles/app.js');
 
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT, ()=>{
-    console.log(`Listening on ${PORT}`);
-})
+app.listen(PORT, ()=>
+    console.log(`Listening on ${PORT}`))
