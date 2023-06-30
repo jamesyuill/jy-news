261cdd92ee3ee1df4682e30a053ba2b4a30d6ffa
James Yuill <jamesyuill@jamess-mbp.lan>
2023-06-30 08:40:23 +0100

    updates task 16 with passing tests for objectwith properties and object type, plus an error test for invalid type username

--- __tests__/topics.test.js
+++ __tests__/topics.test.js
@@ -552,3 +552,37 @@ describe('FEATURE: GET /api/articles/:article_id (comment_count', ()=>{
     })
   })
 })
+
+describe.only('ADVANCED: GET /api/users/:username', ()=>{
+  test('200: responds with a user object', ()=>{
+    return request(app)
+    .get('/api/users/lurker')
+    .expect(200)
+    .then(({body})=>{
+      const { user } = body
+      expect(typeof user).toBe('object')
+    })
+  })
+  test('200: responds with a user object with the correct properties', ()=>{
+    const exampleObject = {
+      username: 'lurker',
+      name: 'do_nothing',
+      avatar_url: 'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png'
+    }
+    return request(app)
+    .get('/api/users/lurker')
+    .expect(200)
+    .then(({body})=>{
+      const { user } = body
+      expect(user).toMatchObject(exampleObject)
+    })
+  })
+  test('404: responds with error if username is invalid type', ()=>{
+    return request(app)
+    .get('/api/users/9')
+    .expect(404)
+    .then(({body})=>{
+      expect(body.msg).toBe('Not found');
+    })
+  })
+})
--- serverFiles/routes/users-router.js
+++ serverFiles/routes/users-router.js
@@ -1,9 +1,15 @@
-const getAllUsers = require('../controllers/users.controllers');
+const {getAllUsers, getUserByUsername} = require('../controllers/users.controllers');
 
 const usersRouter = require('express').Router()
 
 usersRouter
   .route('/')
   .get(getAllUsers);
+
+  usersRouter
+  .route('/:username')
+  .get(getUserByUsername);
+
+  
 
 module.exports = usersRouter;
