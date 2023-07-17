04753604ce774f4cc33f6c0cf0905b8014635bb8
James Yuill <jamesyuill@jamess-mbp.lan>
2023-07-17 09:41:33 +0100

    allow cross origin resource sharing

--- serverFiles/app.js
+++ serverFiles/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const apiRouter = require('../serverFiles/routes/api-router.js')
+const apiRouter = require('../serverFiles/routes/api-router.js');
 const { handlePsqlErrors, handleCustomErrors } = require('./errorhandlers');
 
+const cors = require('cors');
+
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
